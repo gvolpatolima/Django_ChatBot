e65b6280bf5f2d1d8a54409ef1ae244bb28690ee
Volpato <gvolpatolima@gmail.com>
2023-04-17 01:43:09 -0300

    Starting to interact with ai

--- Chatbot_Django/chat/views.py
+++ Chatbot_Django/chat/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+import openai
 
 def index(request):
     return render(request, 'chat/index.html')
 
+def getResponse(request):
+    userMessage = request.GET.get('userMessage')
+    return HttpResponse(userMessage)
