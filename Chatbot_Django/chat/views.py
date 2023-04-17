0fcc1fb642fe2afdfb502788ebc63a478bbe7ef1
Volpato <gvolpatolima@gmail.com>
2023-04-16 21:07:27 -0300

    adding index.html

--- Chatbot_Django/chat/views.py
+++ Chatbot_Django/chat/views.py
@@ -1,12 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
     return HttpResponse("This is my first url")
 
-def specific(request):
-    numre = 55
-    return HttpResponse("numre")
-
-def article(request, particle_id):
-    return HttpResponse(particle_id)
+def article(request):
+    return render(request, 'blog/index.html')
